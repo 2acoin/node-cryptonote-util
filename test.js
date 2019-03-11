852172cf6537bed239cdb31118a3e2a1526dd99e
2ACoin Miner <miner@2acoin.org>
2019-03-11 15:16:31 -0500

    Fix Test Script for 2ACoin

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const cnUtil = require('./')
 const Buffer = require('safe-buffer').Buffer
 const assert = require('assert')
 
-var validAddressPrefix = 3914525
-var addressPrefix = cnUtil.address_decode(new Buffer('TRTLuxN6FVALYxeAEKhtWDYNS9Vd9dHVp3QHwjKbo76ggQKgUfVjQp8iPypECCy3MwZVyu89k1fWE2Ji6EKedbrqECHHWouZN6g'))
+var validAddressPrefix = 0x1fcdee
+var addressPrefix = cnUtil.address_decode(new Buffer('gunsGwwGCqEdXYkT7fHxGVZT6aR3bVL3tbW6uDAAr3CQJbKGYgtXDRiGA8Xpm4UKikNXUZ4WATFkCUu62zxoDDM155TYthufc9'))
 console.log("Address Prefix: ", addressPrefix)
 
 assert.deepEqual(validAddressPrefix, addressPrefix)
